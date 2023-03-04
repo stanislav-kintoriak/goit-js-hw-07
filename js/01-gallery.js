64147eea7e50a586fec11b7dde40724b349a73cb
Stanislav Kintoriak <kintoriak@gmail.com>
2023-03-04 20:56:07 +0200

    commit for check

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,81 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const picturesContainer = document.querySelector('.gallery');
+
+
+// creating markup
+
+const picturesMarkup = createPicturesCardsMarkup(galleryItems);
+
+picturesContainer.insertAdjacentHTML('afterbegin', picturesMarkup);
+
+function createPicturesCardsMarkup(galleryItems){
+
+return galleryItems.map(({preview, original, description}) => {
+    return `<div class="gallery__item">
+    <a class="gallery__link" href="${original}">
+      <img
+        class="gallery__image"
+        src="${preview}"
+        data-source="${original}"
+        alt="${description}"
+      />
+    </a>
+  </div>`;
+}).join('');
+
+}
+
+
+// check what we click
+
+picturesContainer.addEventListener('click', onPictureClick);
+
+function onPictureClick(evt){
+
+const isPictureClicked = evt.target.classList.contains("gallery__image")
+
+if(!isPictureClicked){
+    return;
+}
+
+noDefault(evt);
+
+
+
+// image for modal
+
+const instance = basicLightbox.create (
+`<img src="${evt.target.dataset.source}" width="800" height="600">`, {onShow: () => {
+
+    document.addEventListener('keydown', modalClosingHandler )
+}, onClose: () => {
+    document.removeEventListener('keydown', modalClosingHandler )
+},
+}
+
+);
+
+
+instance.show();
+
+
+// function - closing modal by escape
+
+function modalClosingHandler(evt){
+
+    if(evt.code === 'Escape'){
+        instance.close();
+    }
+
+}
+
+// function - prevent default
+
+function noDefault(evt){
+evt.preventDefault();
+}
+
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,28 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+
+const picturesContainerUl = document.querySelector('.gallery');
+
+
+// creating markup
+
+const picturesMarkupInUl = createPicturesCardsMarkupForUl(galleryItems);
+
+picturesContainerUl.insertAdjacentHTML('afterbegin', picturesMarkupInUl);
+
+function createPicturesCardsMarkupForUl(galleryItems){
+
+return galleryItems.map(({preview, original, description}) => {
+    return `<li>
+    <a class="gallery__item" href="${original}">
+    <img class="gallery__image" src="${preview}" alt="${description}" 
+    />
+  </a>
+  </li>`
+}).join('');
+
+}
+
+const lightbox = new SimpleLightbox('.gallery a', {captionsData: 'alt', captionDElay: 250});
